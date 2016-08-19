f152c3b854c3c8ed1ca32eb3254c049d1b78ddd3
dwanmight <dwanmight@gmail.com>
2016-08-19 19:47:21 +0300

    write onCreate && onCreateView

--- app/src/main/java/com/junior/dwan/sharepictures/ShareFragment.java
+++ app/src/main/java/com/junior/dwan/sharepictures/ShareFragment.java
@@ -1,9 +1,27 @@
 package com.junior.dwan.sharepictures;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Might on 19.08.2016.
  */
 public class ShareFragment extends Fragment {
+    private Letter mLetter;
+
+    @Override
+    public void onCreate(Bundle  savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup parent,Bundle savedInstanceState){
+        View v=inflater.inflate(R.layout.activity_share,parent,false);
+
+        return v;
+    }
 }
